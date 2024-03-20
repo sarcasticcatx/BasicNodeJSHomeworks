e6351ae5c2f7a0b168b591d2aa9fd461d4e6a0fa
Teodora <150371708+sarcasticcatx@users.noreply.github.com>
2024-03-20 15:55:16 +0100

    Delete Exercize#1 directory

--- Exercize#1/utils.js
+++
@@ -1,8 +0,0 @@
-import { fileURLToPath } from "node:url";
-import path from "node:path";
-
-export const createPath = (pathFragments) => {
-  const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
-  return path.join(__dirname, ...pathFragments);
-};
