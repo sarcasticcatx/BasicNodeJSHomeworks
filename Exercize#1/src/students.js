c1098f7695d0a088e039e98f8571e92295d31c34
Teodora <150371708+sarcasticcatx@users.noreply.github.com>
2024-03-20 15:18:24 +0100

    Update students.js

--- Exercize#1/src/students.js
+++ Exercize#1/src/students.js
@@ -12,14 +12,12 @@ const saveStudents = async (students) => {
 export const getAllStudents = async (filters) => {
   let students = await DataService.readJSONFile(STUDENTS_PATH);
 
+  //filter out the gender
   if (filters?.gender) {
-    students = students.filter(
-      (students) => students.gender === "filters.gender"
-    );
+    students = students.filter((student) => student.gender === "Female");
 
-    if (filters.sortBy) {
-      students = students.sort((a, b) => a.age - b.age);
-    }
+    //sort out by average grade or age
+    students = students.sort((a, b) => a.age - b.age);
   }
 
   return students;
@@ -29,7 +27,7 @@ export const getAllStudents = async (filters) => {
 export const getAllStudentsById = async (studentsId) => {
   const students = await getAllStudents();
 
-  const foundStudent = students.find((student) => student.id === studentsId);
+  const foundStudent = students.find((student) => studentsId === student.id);
 
   if (!foundStudent) throw new Error("Student not found");
 
